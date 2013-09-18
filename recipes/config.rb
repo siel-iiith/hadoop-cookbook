5c90ab43be7a459211ed9c1fe7e12b52efb29d7d
Shashank Sahni <shredder12@gmail.com>
2013-09-18 17:20:54 +0530

    Moved conf generation to config.rb
    Signed-off-by: Shashank Sahni <shredder12@gmail.com>

---
+++ recipes/config.rb
@@ -0,0 +1,28 @@
+template "/etc/hadoop/mapred-site.xml" do
+	source "mapred-site.xml.erb"
+	action :create
+end
+
+template "/etc/hadoop/core-site.xml" do
+	source "core-site.xml.erb"
+	variables(
+		:location_addr => node['hadoop']['namenode']
+		)
+	action :create
+	only_if { node['hadoop']['dfs']['uri'] == "hdfs://" }
+end
+
+template "/etc/hadoop/core-site.xml" do
+	source "core-site.xml.erb"
+	variables(
+		:location_addr => node['hadoop']['s3']['bucket']
+		)
+	action :create
+	only_if { node['hadoop']['dfs']['uri'] == 's3://' }
+end
+
+template "/etc/hadoop/hdfs-site.xml" do
+	source "hdfs-site.xml.erb"
+	action :create
+	only_if { node['hadoop']['dfs']['uri'] == 'hdfs://' }
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,33 +1,6 @@
 include_recipe "hadoop::prepare"
 
-template "/etc/hadoop/mapred-site.xml" do
-	source "mapred-site.xml.erb"
-	action :create
-end
-
-template "/etc/hadoop/core-site.xml" do
-	source "core-site.xml.erb"
-	variables(
-		:location_addr => node['hadoop']['namenode']
-		)
-	action :create
-	only_if { node['hadoop']['dfs']['uri'] == "hdfs://" }
-end
-
-template "/etc/hadoop/core-site.xml" do
-	source "core-site.xml.erb"
-	variables(
-		:location_addr => node['hadoop']['s3']['bucket']
-		)
-	action :create
-	only_if { node['hadoop']['dfs']['uri'] == 's3://' }
-end
-
-template "/etc/hadoop/hdfs-site.xml" do
-	source "hdfs-site.xml.erb"
-	action :create
-	only_if { node['hadoop']['dfs']['uri'] == 'hdfs://' }
-end
+include_recipe "hadoop::config"
 
 include_recipe "hadoop::jobtracker" if node['hadoop']['role'] == 'jobtracker'
 include_recipe "hadoop::tasktracker" if node['hadoop']['role'] == 'tasktracker'
